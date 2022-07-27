78c56915eaff841e6d195c65a9535c8192f4e797
Edward Mwangi <69647173+EddyMunga@users.noreply.github.com>
2022-07-27 15:29:00 -0700

    Add files via upload

---
+++ Portfolio-Two/script.js
@@ -0,0 +1,38 @@
+const hamburgerMenu =
+document.querySelector("#hamburger-menu");
+const overlay =
+document.querySelector("#overlay");
+const nav1 = document.querySelector("#nav-1");
+const nav2 = document.querySelector("#nav-2");
+const nav3 = document.querySelector("#nav-3");
+const nav4 = document.querySelector("#nav-4");
+const nav5 = document.querySelector("#nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+//control navigation animation
+function navAnimation(val1, val2) { 
+    navItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${val1}-${i + 1}`, 
+        `slide-${val2}-${i + 1}`);
+    });
+}
+function toggleNav(){
+    //Toggle: Hamburger Opne/Close
+    hamburgerMenu.classList.toggle("active");
+    overlay.classList.toggle("overlay-active");
+    if (overlay.classList.contains("overlay-active")) {
+        overlay.classList.replace("overlay-slide-left",
+        "overlay-slide-right");
+
+        navAnimation("out", "in");
+    } else {
+        overlay.classList.replace("overlay-slide-right",
+        "overlay-slide-left");
+        navAnimation("in", "out");
+    }
+}
+//Event Listeners
+hamburgerMenu.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+    nav.addEventListener("click", toggleNav);
+});
